aaab2a2e88f216093b1cd7a13f415030ee09ba1d
Shishirbaral1 <shisirbaral3@gmail.com>
2026-01-31 17:34:33 -0500

    Add files via upload

---
+++ Week-1/Major.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Shishir Baral" << endl;
+    cout << "Class no: 6.67" << endl;
+    cout << "Major: Comouter science" << endl;
+    return 0;
+}
---
+++ Week-1/Number.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number;
+    
+    cout << "Enter a number: ";
+    cin >> number;
+    
+    cout <<"You typed: " << number << endl;
+    return 0;
+    
+}
